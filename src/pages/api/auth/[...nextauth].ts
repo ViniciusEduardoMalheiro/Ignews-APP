4328b0075df784afd28dcb39e6d20c7a456a3b71
Vinicius <vinicius.mlhro@gmail.com>
2022-04-25 23:12:56 -0300

    evitando duplicações

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import { signIn } from 'next-auth/react';
 import { fauna } from '../../../services/fauna';
-import { query as q } from 'faunadb';
+import { If, query as q } from 'faunadb';
 
 export default NextAuth ({
    providers: [
@@ -19,14 +19,36 @@ export default NextAuth ({
   callbacks: {
     async signIn({ user, account, profile }) {
       const { email } = user;
+
+     try {
       await fauna.query(
+       q.If(
+         q.Not(
+           q.Exists(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+            )
+          )
+        ),
         q.Create(
           q.Collection('users'),
           {data: {email} }
+        ),
+        q.Get(
+          q.Match(
+            q.Index('user_by_email'),
+            q.Casefold(user.email)
+        )
         )
       )
+    )
 
       return true;
-    }
+     } catch {
+       return false;
+     }
+
+    },
   }
 })
