38f38b6a465951b7822f8a6e84b89fd95d832eeb
Vinicius <vinicius.mlhro@gmail.com>
2022-05-04 23:30:34 -0300

    Finalização do app Ignews

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -83,6 +83,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
        props: {
            post,
            
-       }
+       },
+       redirect: 60 * 30, //30 min
    }
 }
